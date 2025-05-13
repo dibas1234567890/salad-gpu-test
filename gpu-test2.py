47ee8005ef2b0b15c602f77d427e1b09a2704540
dibas1234567890 <dibaspratap@gmail.com>
2025-05-13 15:04:27 +0545

    Add: gputest2

--- gpu-test2.py
+++ gpu-test2.py
@@ -1,2 +1,14 @@
-from outlines import models
+from outlines import models, generate
+from pydantic import BaseModel
+
 model = models.vllm("google/gemma-3-4b-it",dtype="float16",trust_remote_code=True)
+class User(BaseModel):
+    name: str
+    last_name: str
+    id: int
+
+generator = generate.json(model, User)
+result = generator(
+    "Create a user profile with the fields name, last_name and id"
+)
+print(result)
